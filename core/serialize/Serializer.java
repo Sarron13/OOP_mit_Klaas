296d628c3ba6f096cf0561b5ea248f82aa0f53c0
Sarron13 <lukas.mail13@gmail.com>
2021-04-13 15:41:55 +0200

    Implemented Serializable-Interface and created File-Creater class.

--- core/serialize/Serializer.java
+++ core/serialize/Serializer.java
@@ -1,5 +1,13 @@
 package core.serialize;
 
-public class Serializer {
+import java.io.Serializable;
+
+public class Serializer implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    public static int serializeData() {
+        return 0;
+    }
 
 }
