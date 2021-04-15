49b89a8da365e4f8ac648b104775705b465036de
aeropagz <klaas.pelzer@gmx.de>
2021-04-15 13:51:44 +0200

    added source in maven

--- README.md
+++
@@ -1,1 +0,0 @@
-# OOP_mit_Klaas
--- src/main/java/com/fhkiel/oopproject/rest/CharacterController.java
+++ src/main/java/com/fhkiel/oopproject/rest/CharacterController.java
@@ -2,9 +2,11 @@ package com.fhkiel.oopproject.rest;
 
 
 import java.util.List;
+import java.util.UUID;
 
 import com.fhkiel.oopproject.container.CharacterContainer;
 import com.fhkiel.oopproject.model.Character;
+import com.fhkiel.oopproject.model.CharacterNotFound;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -13,13 +15,32 @@ public class CharacterController {
     private final CharacterContainer container = CharacterContainer.getInstance();
 
     @PostMapping(path = "/character", consumes = "application/json", produces = "application/json")
-    public void addPerson(@RequestBody Character c){
+    public void addCharacter(@RequestBody Character c) {
         container.addCharacter(c);
     }
 
     @GetMapping("/character")
-    public List<Character> getPersons(){
+    public List<Character> getCharacters() {
         return container.getAllCharacters();
+    }
+
+    @GetMapping("/character/{id}")
+    public Character getCharacter(@PathVariable UUID id) {
+        if (container.findById(id) != null)
+            return container.findById(id);
+        else
+            throw new CharacterNotFound(id);
+    }
+
+    @PutMapping("/character")
+    public void updateCharacter(@RequestBody Character c){
+        if(container.updateCharacter(c) == null)
+            throw new CharacterNotFound(c.getId());
+    }
+
+    @GetMapping("/character/search")
+    public List<Character> searchCharacters(@RequestParam String term) {
+        return container.search(term);
     }
 }
 
