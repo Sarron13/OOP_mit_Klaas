abf04adf2c15334991fcaa1ddac1fdedde43824d
Sarron13 <lukas.mail13@gmail.com>
2021-05-29 14:44:42 +0200

    Added Java-Doc to every Method in LordOfRingsChar and StarWarsChar.

--- src/main/java/com/fhkiel/oopproject/model/LordOfRingsChar.java
+++ src/main/java/com/fhkiel/oopproject/model/LordOfRingsChar.java
@@ -2,34 +2,63 @@ package com.fhkiel.oopproject.model;
 
 import com.fasterxml.jackson.annotation.JsonTypeName;
 
+//TODO: Is Reload after editing a Character ok?
+
 /**
  * <strong>Class-Description:</strong><br/>
- * The class "LordOfRingsChar" is a concrete childclass of the abstract
+ * The class "LordOfRingsChar" is a concrete child class of the abstract
  * Character and represents Characters from the LotR-Universe.
  */
 @JsonTypeName("LotR")
 public class LordOfRingsChar extends Character {
     private String favoriteTobacco;
 
+    /**
+     * Creates an instance of a Lord-of-the-Rings-Character by using the {@link Character#Character Constructor} of the
+     * {@link Character Character Class}.
+     * @param firstname Type: {@link String}
+     * @param lastname Type: {@link String}
+     * @param age Type: int
+     * @param tobacco Type: {@link String}
+     */
     public LordOfRingsChar(String firstname, String lastname, int age, String tobacco) {
         super(firstname, lastname, age);
         this.setFavoriteTobacco(tobacco);
     }
 
+    /**
+     * Updates the attributes of the current instance of {@link LordOfRingsChar} by using the temporary created updated
+     * version of itself.
+     * @param c Type: {@link LordOfRingsChar}
+     * @return The updated instance of the {@link LordOfRingsChar Lord-of-the-Rings-Character}
+     */
     public LordOfRingsChar update(LordOfRingsChar c){
         this.setFirstname(c.getFirstname()).setLastname(c.getLastname()).setAge(c.getAge());
         this.setFavoriteTobacco(c.getFavoriteTobacco());
         return this;
     }
 
+    /**
+     * @return The {@link #favoriteTobacco favorite tobacco} of the current instance of {@link LordOfRingsChar} as {@link String}
+     */
     public String getFavoriteTobacco() {
         return favoriteTobacco;
     }
 
+    /**
+     * @param favoriteTobacco Type: {@link String}
+     */
     public void setFavoriteTobacco(String favoriteTobacco) {
         this.favoriteTobacco = favoriteTobacco;
     }
 
+    /**
+     * This method is used to perform the {@link #update update process} by using a {@link UpdateVisitor Visitor} and
+     * automatically upcast to a {@link LordOfRingsChar}.
+     * @param v Type: {@link UpdateVisitor}
+     * @param c Type: {@link Character}
+     * @return The updated instance of the {@link LordOfRingsChar}
+     */
     @Override
     public Character acceptUpdater(UpdateVisitor v, Character c) {
         return v.visit(this, c);
--- src/main/java/com/fhkiel/oopproject/model/StarWarsChar.java
+++ src/main/java/com/fhkiel/oopproject/model/StarWarsChar.java
@@ -4,33 +4,60 @@ import com.fasterxml.jackson.annotation.JsonTypeName;
 
 /**
  * <strong>Class-Description:</strong><br/>
- * The class "StarWarsChar" is a concrete childclass of the abstract
- * Character and representsffjdk Characters from the Star-Wars-Universe.
+ * The class "StarWarsChar" is a concrete child class of the abstract
+ * Character and represents Characters from the Star-Wars-Universe.
  */
 
 @JsonTypeName("StarWars")
 public class StarWarsChar extends Character {
     private String spaceship;
 
+    /**
+     * Creates an instance of a Star-Wars-Character by using the {@link Character#Character Constructor} of the
+     * {@link Character Character Class}.
+     * @param firstname Type: {@link String}
+     * @param lastname Type: {@link String}
+     * @param age Type: int
+     * @param spaceship Type: {@link String}
+     */
     public StarWarsChar(String firstname, String lastname, int age, String spaceship) {
         super(firstname, lastname, age);
         this.setSpaceship(spaceship);
     }
 
+    /**
+     * Updates the attributes of the current instance of {@link StarWarsChar} by using the temporary created updated
+     * version of itself.
+     * @param c Type: {@link StarWarsChar}
+     * @return The updated instance of the {@link StarWarsChar Star-Wars-Character}
+     */
     public StarWarsChar update(StarWarsChar c) {
         this.setFirstname(c.getFirstname()).setLastname(c.getLastname()).setAge(c.getAge());
         this.setSpaceship(c.getSpaceship());
         return this;
     }
 
+    /**
+     * @return The {@link #spaceship} of the current instance of {@link StarWarsChar} as {@link String}
+     */
     public String getSpaceship() {
         return spaceship;
     }
 
+    /**
+     * @param spaceship Type: {@link String}
+     */
     public void setSpaceship(String spaceship) {
         this.spaceship = spaceship;
     }
 
+    /**
+     * This method is used to perform the {@link #update update process} by using a {@link UpdateVisitor Visitor} and
+     * automatically upcast to a {@link StarWarsChar}.
+     * @param v Type: {@link UpdateVisitor}
+     * @param c Type: {@link Character}
+     * @return The updated instance of the {@link StarWarsChar}
+     */
     @Override
     public Character acceptUpdater(UpdateVisitor v, Character c) {
         return v.visit(this, c);
