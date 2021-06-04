bef1d498d8c6e31a7b3631b38808c3c105e52c7d
aeropagz <klaas.pelzer@gmx.de>
2021-06-04 12:13:51 +0200

    bux fix and prettier print

--- src/main/java/com/fhkiel/oopproject/container/ContainerMain.java
+++ src/main/java/com/fhkiel/oopproject/container/ContainerMain.java
@@ -5,37 +5,37 @@ import com.fhkiel.oopproject.model.LordOfRingsChar;
 import com.fhkiel.oopproject.model.StarWarsChar;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class ContainerMain {
 
     public static void main(String[] args) {
         CharacterContainer container = CharacterContainer.getInstance();
-
+        System.out.println("Demo Programm\n");
+        System.out.println("Add Lukas Skywalker");
         // Add
         StarWarsChar c = new StarWarsChar("Lukas", "Skywalker", 25, "X-Wing");
         container.addCharacter(c);
+        System.out.println("Add Bilbo Beutlin");
         container.addCharacter(new LordOfRingsChar("Bilbo", "Beutlin", 56, "Lucky"));
 
         // Get all
+        System.out.println("Get all chars: ");
         ArrayList<Character> allChars = container.getAllCharacters();
+        System.out.println(allChars.toString() + "\n");
 
         // Find
-        Character c2 = container.findByID(allChars.get(0).getId());
-
-        // Search
-        List<Character> cList = container.search("Skywalker");
-        List<Character> cList2 = container.search("Vader");
-
-        // update c2
-        System.out.println("Old:\n" + c2 +"\n");
-        StarWarsChar newC2 = new StarWarsChar("Luke", "Skywalker", 30, "X-Wing");
-        System.out.println("Updated:\n" + container.updateCharacter((Character) newC2) + "\n");
+        LordOfRingsChar c2 = (LordOfRingsChar) container.findByID(container.search("Bilbo").get(0).getId());
+        System.out.println("Find Bilbo Beutlins ID and get Bilbo");
+        System.out.println(c2 + "\n");
+        System.out.println("change Bilbos Age and update");
+        c2.setAge(102);
+        System.out.println("Updated Bilbo:\n" + container.updateCharacter(c2));
+        System.out.println("Updated List:\n" + container + "\n");
 
         // delete c2
-        System.out.println("Old list:\n" + container.toString() + "\n");
-        container.deleteByID(c2.getId());
-        System.out.println("New list:\n" + container.toString() + "\n");
+        System.out.println("Delete Lukas Skywalker");
+        container.deleteByID(c.getId());
+        System.out.println("New list without Skywalker:\n" + container + "\n");
     }
 
 }
